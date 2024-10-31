2ae0bf3eaa8592ff3d01bdc5bf151905824042af
Coull <accoull@amazon.com>
2024-10-31 11:50:35 -0700

    correct typing check

--- src/autoqasm/api.py
+++ src/autoqasm/api.py
@@ -610,8 +610,9 @@ def _get_gate_args(f: Callable) -> aq_program.GateArgs:
 
         if param.annotation == aq_instructions.QubitIdentifierType:
             gate_args.append_qubit(param.name)
-        elif isinstance(param.annotation, float) or any(
-            isinstance(type_, float) for type_ in get_args(param.annotation)
+        elif param.annotation == float or any(  # noqa: E721
+            type_ == float  # noqa: E721
+            for type_ in get_args(param.annotation)
         ):
             gate_args.append_angle(param.name)
         else:
