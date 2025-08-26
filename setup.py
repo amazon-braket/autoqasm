3fe1e9217ad91f4751e84ae140252d5c64e917b8
Ryan Shaffer <3620100+rmshaffer@users.noreply.github.com>
2025-08-26 13:14:13 -0400

    update supported python versions

--- setup.py
+++ setup.py
@@ -20,7 +20,7 @@ setup(
     name="autoqasm",
     version="0.1.2",
     license="Apache License 2.0",
-    python_requires=">= 3.9",
+    python_requires=">= 3.10",
     packages=find_namespace_packages(where="src", exclude=("test",)),
     package_dir={"": "src"},
     install_requires=[
@@ -74,8 +74,9 @@ setup(
         "Natural Language :: English",
         "License :: OSI Approved :: Apache Software License",
         "Programming Language :: Python",
-        "Programming Language :: Python :: 3.9",
         "Programming Language :: Python :: 3.10",
         "Programming Language :: Python :: 3.11",
+        "Programming Language :: Python :: 3.12",
+        "Programming Language :: Python :: 3.13",
     ],
 )
