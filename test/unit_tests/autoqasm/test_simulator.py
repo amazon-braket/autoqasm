332b3cd0e49932d9f8a7dd26b2bf3a09bf9d031b
Tim <yitchen@amazon.com>
2025-09-15 20:44:39 -0400

    remove asserting error message

--- test/unit_tests/autoqasm/test_simulator.py
+++ test/unit_tests/autoqasm/test_simulator.py
@@ -150,7 +150,7 @@ def test_qubit_register_indexing():
     assert result["__bit_0__"] == ["01"]
 
 
-@pytest.mark.parametrize("invalid_index", ("1.23", "$1"))
+@pytest.mark.parametrize("invalid_index", ("1.23", "$1", "pi", "{0, pi}"))
 def test_qubit_register_invalid_index(invalid_index):
     qasm = f"""
         OPENQASM 3.0;
@@ -158,7 +158,7 @@ def test_qubit_register_invalid_index(invalid_index):
         x __qubits__[{invalid_index}];
         bit[2] __bit_0__ = measure __qubits__;
     """
-    with pytest.raises(TypeError, match=r"tuple indices must be integers or slices"):
+    with pytest.raises(TypeError):
         NativeInterpreter(Simulation(1, 1, 1)).simulate(qasm)
 
 
