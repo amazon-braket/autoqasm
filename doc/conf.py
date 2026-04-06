a3622c4c44318fb9a907310e3add2b5f915b75ea
Ryan Shaffer <3620100+rmshaffer@users.noreply.github.com>
2026-04-06 17:15:46 -0400

    fix: ensure sphinx pdf build passes  (#100)
    * fix: docs pdf build
    
    * add pygments styles
    
    * use LatexFormatter for pygments styles

--- doc/conf.py
+++ doc/conf.py
@@ -3,11 +3,14 @@
 import datetime
 from importlib.metadata import version
 
+from pygments.formatters import LatexFormatter
+
 # Sphinx configuration below.
 project = "autoqasm"
 version = version(project)
 release = version
 copyright = f"{datetime.datetime.now().year}, Amazon.com"
+author = "Amazon Web Services"
 
 extensions = [
     "sphinxcontrib.apidoc",
@@ -34,6 +37,20 @@ html_theme = "sphinx_rtd_theme"
 htmlhelp_basename = f"{project}doc"
 
 language = "en"
+
+# LaTeX configuration
+_pygments_style_defs = LatexFormatter().get_style_defs()
+
+latex_elements = {
+    "preamble": rf"""
+% Define commands for Dirac notation (used in quantum computing notebooks)
+\providecommand{{\ket}}[1]{{\left|#1\right\rangle}}
+\providecommand{{\bra}}[1]{{\left\langle#1\right|}}
+\providecommand{{\braket}}[2]{{\left\langle#1\middle|#2\right\rangle}}
+% Pygments style definitions for syntax-highlighted code in nbsphinx cells
+{_pygments_style_defs}
+""",
+}
 
 napoleon_use_rtype = False
 
